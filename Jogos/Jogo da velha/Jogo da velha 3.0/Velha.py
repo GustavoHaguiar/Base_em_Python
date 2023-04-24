b274ab7fdf4bcc5dbd23bd949ebc9c28c5f97c59
Gustavo Henrique <gustavohgaguiar@yahoo.com>
2023-04-24 00:48:17 -0300

    jogo da velha 3.0 ja iniciado porém com diversos erros por conta da modularização

--- Jogos/Jogo da velha/Jogo da velha 3.0/Velha.py
+++ Jogos/Jogo da velha/Jogo da velha 3.0/Velha.py
@@ -1,3 +1,53 @@
-from functions import showInfo
+import functions as velha
 
-showInfo()
+
+# declarações
+
+matriz = [
+            [None, None, None],
+            [None, None, None],
+            [None, None, None]
+]
+
+fimJogo = False
+player1 = ''
+player2 = ''
+vez = 0
+
+
+#informações e introdução ao jogo
+velha.showInfo()
+
+# entrada do jogador1
+player1 = str(input("Player1 [X/O]: ")).upper()
+
+# definição dos jogadores
+player1, player2 = velha.verifyPlayers(player1, player2)
+
+# resultado da correção
+print(f"Player 1: {player1}\nPlayer 2: {player2}\n")
+
+# loop para fazer jogo funcionar
+
+while fimJogo == False:
+    if vez % 2 == 0:
+        jgd = player1
+    else:
+        jgd = player2
+    
+    # imprime o mapa
+    print("Escolha uma posição: \n")
+    velha.showMap(matriz, type=True)
+    print("\nEstado jogo:")
+    velha.showMap(matriz)
+
+    # realiza a jogada
+    matriz = velha.jogada(matriz, jgd)
+
+    # resultado da jogada
+    velha.showMap(matriz)
+
+    # verifica o fim de jogo
+    fimJogo = velha.verifyEnd(matriz)
+
+# ALGUNS BUGS PARA CORRIGIR
--- Jogos/Jogo da velha/Jogo da velha 3.0/functions/__init__.py
+++ Jogos/Jogo da velha/Jogo da velha 3.0/functions/__init__.py
@@ -1,7 +1,34 @@
 from time import sleep
 
-def showMap(type):
-    pass
+def showMap(matriz, type=False):
+    """mostra o mapa <matriz> e tem a opção de mostrá-la vazia caso type == None, 
+    no caso de type == True vai ser impresso o mapa do jogo, False retorna a própria matriz.
+    Essa função não funciona caso o número de caracteres nas colunas/linhas não seja proporcional."""
+    if type == None:
+        l = len(matriz)
+        c = len(matriz[0])
+        new_matriz = []
+        for i in range(l):
+            linha = []
+            for j in range(c):
+                linha.append(None)
+            new_matriz.append(linha)
+    elif type == True:
+        l = len(matriz)
+        c = len(matriz[0])
+        new_matriz = []
+        linha = []
+        new_matriz = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+        ]
+    if type != False:
+        for item in new_matriz:
+            print(item)
+    else:
+        for item in matriz:
+                print(item)
 
 def showInfo():
     print("---------------------- Jogo da Velha ----------------------")
@@ -17,15 +44,80 @@ def showInfo():
     print("\t\t\tComeçou!")
     print("-----------------------------------------------------------")
 
-def verifyCol():
-    pass
+def verifyCol(matriz):
+    for i in range(len(matriz)):
+        if matriz[0][i] == matriz[1][i] == matriz[2][i] != None:
+            return True
+    return False
+    
+def verifyLine(matriz):
+    for i in range(len(matriz[0])):
+        if matriz[i][0] == matriz[i][1] == matriz[i][2] != None:
+            return True
+    return False
 
-def verifyLine():
-    pass
+def verifyDiag(matriz):
+    if matriz[0][0] == matriz[1][1] == matriz[2][2] != None:
+        return True
+    elif matriz[2][0] == matriz[1][1] == matriz[0][2] != None:
+        return True
+    return False
 
-def verifyDiag():
-    pass
+def verifyVelha(matriz):  ## verifica Velha
+    h = 0
+    for i in range(3):
+        for j in range(3):
+            if None == matriz[i][j]:
+                h += 1
+    if h >= 1:
+        return False
+    return True
 
-def verifyVelha():
-    pass
+def verifyPlayers(player1, player2):
+    if player1 == "X":
+        player2 = "O"
+    elif player1 == "O":
+        player2 = "X"
+    if player1 not in "XxOo":
+        player1 = "X"
+        player2 = "O"
+    return player1, player2
 
+def jogada(matriz, player):
+    while True: # escolher posição certa    
+        posição = int(input("Digite a posição: "))
+        if posição < 3 and posição >= 0:
+            pos1 = 0
+            break
+        elif posição < 6 and posição >= 0:
+            pos1 = 1
+            break
+        elif posição < 9 and posição >= 0:
+            pos1 = 2
+            break
+        else:
+            print("Escolha uma posição válida")
+
+        
+        if str(posição) in '036':
+            posição = 0
+        elif str(posição) in '147':
+            posição = 1
+        elif str(posição) in '258':
+            posição = 2
+        
+        if matriz[pos1][posição] == None:
+            matriz[pos1][posição] = player
+            return matriz
+
+def verifyEnd(matriz):
+    fimJogo = False
+    if fimJogo == False:
+        fimJogo = verifyCol(matriz)
+    if fimJogo == False:
+        fimJogo = verifyLine(matriz)
+    if fimJogo == False:
+        fimJogo = verifyDiag(matriz)
+    if fimJogo == False:
+        fimJogo = verifyVelha(matriz)
+    return fimJogo
