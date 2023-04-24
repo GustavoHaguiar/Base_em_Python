87b5d793023fcf43f9e722947038602cb55c39be
Gustavo Henrique <gustavohgaguiar@yahoo.com>
2023-04-23 22:46:47 -0300

    Sem alteração de codigo. adicionado ideias de novos projetos

---
+++ Jogos/Jogo da velha/Jogo da velha 3.0/Velha.py
@@ -0,0 +1,3 @@
+from functions import showInfo
+
+showInfo()
---
+++ Jogos/Jogo da velha/Jogo da velha 3.0/functions/__init__.py
@@ -0,0 +1,31 @@
+from time import sleep
+
+def showMap(type):
+    pass
+
+def showInfo():
+    print("---------------------- Jogo da Velha ----------------------")
+    print("Regras:\n")
+    print("   - Jogador 1 começa o jogo")
+    print("   - Ganha quem fizer uma fileira tanto na linear com [X/O]\n")
+    print("1")
+    sleep(1)
+    print("2")
+    sleep(1)
+    print("3")
+    sleep(1)
+    print("\t\t\tComeçou!")
+    print("-----------------------------------------------------------")
+
+def verifyCol():
+    pass
+
+def verifyLine():
+    pass
+
+def verifyDiag():
+    pass
+
+def verifyVelha():
+    pass
+
