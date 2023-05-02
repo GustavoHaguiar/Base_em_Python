62fb57b5036520c6aa83794b43538990c28bdacd
Gustavo Henrique <gustavohgaguiar@yahoo.com>
2023-05-02 19:13:46 -0300

    inicio do criptografia 1.0

--- Criptografia/Crypto 1.0/Crypto.py
+++ Criptografia/Crypto 1.0/Crypto.py
@@ -1,1 +1,18 @@
-# não desenvolvido ainda
+from random import randint
+from tkinter import *
+from tkinter import filedialog
+root = Tk()
+abre = filedialog.askopenfile(mode='r', title='Select file')
+print(abre.name)
+root.mainloop()
+
+
+
+
+'''
+def criptografar(arq, key):
+    pass
+
+def descriptografar(arq, key):
+    pass
+'''
